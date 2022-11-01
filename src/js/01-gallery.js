087fa66f7f461ec85d16e6337f4c407395b9fed0
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2022-11-01 22:32:16 +0200

    in progress

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+// ========== Создание и рендер разметки ==========
+
+const createGalleryMarkup = (items) => {
+  const markup = items.map(({ preview, original, description }) => `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}"/>
+              </a>`).join('');
+
+  return markup;
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// ========== Галерея SimpleLightbox ==========
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+  disableRightClick: true,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const currentTime = function(data) {
+    
+};
+
+player.on('timeupdate', currentTime);
+
+console.log(data);
